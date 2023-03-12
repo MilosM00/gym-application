e0d7e6c304424b1626ef837056bfec0502e41106
unknown <milos.milijasevic00@gmail.com>
2023-03-12 13:35:31 +0100

    modal-js updated

---
+++ js/modal.js
@@ -0,0 +1,48 @@
+const modal = document.querySelector(".modal");
+const modalBox = document.querySelector(".modal-box");
+const closeModal = document.querySelector("#close-modal");
+const packName = document.querySelectorAll(".pack-name");
+const packPrice = document.querySelector(".pack-price");
+const packURL = document.querySelector(".pack-url");
+const buttonPrice = document.querySelectorAll(".button-price");
+
+
+const closeModalF = () =>{
+    modal.style.display = "none";
+    modalBox.style.display = "none";
+};
+
+modal.addEventListener("click", () =>{
+    closeModalF();
+});
+
+modalBox.addEventListener("click", () =>{
+    closeModalF();
+});
+
+buttonPrice[0].addEventListener("click", () =>{
+    modal.style.display = "block";
+    modalBox.style.display = "flex";
+    packName[0].textContent = "Lorem";
+    packName[1].textContent = "Lorem";
+    packPrice.textContent = "50";
+    packURL.href = "#";
+});
+
+buttonPrice[1].addEventListener("click", () =>{
+    modal.style.display = "block";
+    modalBox.style.display = "flex";
+    packName[0].textContent = "Ipsum";
+    packName[1].textContent = "Ipsum";
+    packPrice.textContent = "70";
+    packURL.href = "#";
+});
+
+buttonPrice[2].addEventListener("click", () =>{
+    modal.style.display = "block";
+    modalBox.style.display = "flex";
+    packName[0].textContent = "Dolor";
+    packName[1].textContent = "Dolor";
+    packPrice.textContent = "100";
+    packURL.href = "#";
+});
